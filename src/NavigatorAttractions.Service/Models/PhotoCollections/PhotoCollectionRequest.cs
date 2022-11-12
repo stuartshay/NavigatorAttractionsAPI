ee94dbea45ce1cd3f060bb37ae8862f8efa6e8ff
Stuart Shay <sshay@yahoo.com>
2022-11-12 18:51:03 -0500

    Update

---
+++ src/NavigatorAttractions.Service/Models/PhotoCollections/PhotoCollectionRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace NavigatorAttractions.Service.Models.PhotoCollections
+{
+    public class PhotoCollectionRequest
+    {
+        public string? PhotoId { get; set; }
+
+        public string? LastUpdatedDate { get; set; }
+    }
+}
---
+++ src/NavigatorAttractions.Service/Results/PhotoStatusResult.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace NavigatorAttractions.Service.Results
+{
+    public class PhotoStatusResult
+    {
+        public string PhotoId { get; set; }
+
+        public string Status { get; set; }
+    }
+
+    public class PhotoCollectionStatusResult : IEnumerable<PhotoStatusResult>
+    {
+        public List<PhotoStatusResult> PhotoStatusResults { get; set; }
+
+        public PhotoStatusResult this[int index]
+        {
+            get => PhotoStatusResults[index];
+            set => PhotoStatusResults.Insert(index, value);
+        }
+
+        public IEnumerator<PhotoStatusResult> GetEnumerator()
+        {
+            return PhotoStatusResults.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
