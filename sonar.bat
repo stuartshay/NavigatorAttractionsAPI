c1dc41feee5b57765552668815f93e5501fe446d
Stuart Shay <sshay@yahoo.com>
2022-11-20 00:41:33 -0500

    Update

--- sonar.bat
+++ sonar.bat
@@ -1,4 +1,4 @@
-dotnet sonarscanner begin /k:"NavigatorAttractionsAPI" /d:sonar.host.url="http://node-2:9100"  /d:sonar.login="sqp_c6dd5d7a747e1bc7db6247073435b43465b1f168"
+dotnet sonarscanner begin /k:"NavigatorAttractionsAPI" /d:sonar.host.url="http://node-2:9100" /d:sonar.cs.vscoveragexml.reportsPaths=coverage.xml/d:sonar.login="sqp_f87e7196701050d8bab7c6b673d1bd5720d0e7ee"
 dotnet build --no-incremental
 dotnet test --collect "Code Coverage"
-dotnet sonarscanner end /d:sonar.login="sqp_c6dd5d7a747e1bc7db6247073435b43465b1f168"
+dotnet sonarscanner end /d:sonar.login="sqp_f87e7196701050d8bab7c6b673d1bd5720d0e7ee"
